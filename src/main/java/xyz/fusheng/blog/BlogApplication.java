ba3662fc5305941543fe1a4a27e5427cb7756f89
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-03-10 22:29:19 +0800

    Initial commit

---
+++ src/main/java/xyz/fusheng/blog/BlogApplication.java
@@ -0,0 +1,18 @@
+package xyz.fusheng.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author code_fusheng
+ * data: 2020/3/10
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/xyz/fusheng/blog/enums/StateEnums.java
@@ -0,0 +1,51 @@
+/**
+ * FileName: StateEnum
+ * @Author:   fusheng
+ * @Date:     2020/3/10 22:04
+ * Description: 状态码枚举
+ */
+package xyz.fusheng.blog.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum StateEnums {
+    /**
+     * 逻辑删除状态
+     */
+    DELETED(1, "已删除"),
+    NOT_DELETED(0, "未删除"),
+
+    /**
+     * 启用状态
+     */
+    ENABLED(1, "启用"),
+    NOT_ENABLE(0, "未启用"),
+
+    /**
+     * 性别状态
+     */
+    SEX_MAN(1, "男"),
+    SEX_WOMAN(2, "女"),
+
+    /**
+     * 请求访问状态枚举
+     */
+    REQUEST_SUCCESS(1, "请求正常"),
+    REQUEST_ERROR(0, "请求异常"),
+
+    /**
+     * 用户标识。
+     * 0表示管理员，1表示普通用户
+     */
+    ADMIN(0, "管理员"),
+    USER(1, "普通用户");
+
+    private Integer code;
+    private String msg;
+
+    StateEnums(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}

f5343c906ceab4d2dcd80d314bf27acb284d548a
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-03-14 21:49:07 +0800

    fusheng 刚刚完成了 项目进度 --- 启动类格式化

--- src/main/java/xyz/fusheng/blog/BlogApplication.java
+++ src/main/java/xyz/fusheng/blog/BlogApplication.java
@@ -1,12 +1,13 @@
 package xyz.fusheng.blog;
 
-        import org.mybatis.spring.annotation.MapperScan;
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 启动类
+ *
  * @author code_fusheng
  * @Date: 2020/3/10 22:04  --  2020/3/11 15:42 完成了前期工作
  * @Date: 2020/3/12 10:34  --  2020/3/13 0:20  完成了帖子分类
